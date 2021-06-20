96bd34631ed310c61319db7749d9f1403c2d3fcd
Weihang Lo <me@weihanglo.tw>
2021-06-20 20:00:27 +0800

    feat: basic skelecton of Elector

--- src/elector.rs
+++ src/elector.rs
@@ -1,20 +1,72 @@
-use crate::lock::Lock;
-use std::time::Duration;
+use crate::lock::{ElectionRecord, Lock};
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::MicroTime;
+use std::time::{Duration, SystemTime};
 
-pub struct Elector {
+pub struct Config {
     lock: Box<dyn Lock>,
     lease_duration: Duration,
     renew_duration: Duration,
     retry_period: Duration,
+    callbacks: Box<dyn Callbacks>,
     name: String,
 }
 
+pub trait Callbacks {
+    fn on_started_leading(&self);
+    fn on_stopped_leading(&self);
+    fn on_new_leader(&self, identity: &str);
+}
+
+pub struct Elector {
+    cfg: Config,
+    observed_record: ElectionRecord,
+    // TODO: monotonic time or system/OS time?
+    observed_time: SystemTime,
+    // TODO: unnecessary?
+    reported_leader: String,
+}
+
 impl Elector {
-    pub fn new(lock: Box<dyn Lock>) -> Self {
+    pub fn new(cfg: Config) -> kube::Result<Self> {
         todo!()
     }
 
-    pub fn run(&self) {
+    pub async fn run(&self) {
         todo!("should we reinvent `context.Context` concept in Rust?")
+    }
+
+    pub async fn run_or_die(&self) {
+        todo!("should we reinvent `context.Context` concept in Rust?")
+    }
+
+    pub fn is_leader(&self) -> bool {
+        todo!()
+    }
+    pub fn leader_identity(&self) -> &str {
+        self.cfg.lock.lock_identity()
+    }
+
+    pub fn check(&self) -> kube::Result<()> {
+        todo!()
+    }
+
+    async fn acquire(&self) -> bool {
+        todo!()
+    }
+
+    async fn renew(&self) {
+        todo!()
+    }
+
+    async fn release(&self) -> bool {
+        todo!()
+    }
+
+    async fn acquire_or_renew(&mut self) -> bool {
+        todo!()
+    }
+
+    fn report_transition_if_needed(&mut self) {
+        todo!()
     }
 }
