aec3a64d2203a78689ce2b017f427b514c3a5413
Weihang Lo <me@weihanglo.tw>
2021-06-20 20:44:01 +0800

    feat: acquire_or_renew helper

--- src/elector.rs
+++ src/elector.rs
@@ -99,7 +99,74 @@ impl Elector {
     }
 
     async fn acquire_or_renew(&mut self) -> bool {
-        todo!()
+        use kube::{error::ErrorResponse, Error};
+
+        let now = SystemTime::now();
+        let mut new_record = ElectionRecord {
+            acquire_time: Some(MicroTime(now.clone().into())),
+            renew_time: Some(MicroTime(now.clone().into())),
+            lease_duration_seconds: Some(self.cfg.lease_duration.as_secs() as i32),
+            // TODO: handle (as_secs -> u64) -> i32
+            holder_identity: Some(self.cfg.lock.lock_identity().to_string()),
+            ..Default::default()
+        };
+
+        // 1. Obtain or create the `ElectionRecord`
+        //
+        // - Do not propagate error here. Just log them.
+        // - Create new record if error emerges, except error 404.
+        match self.cfg.lock.get().await {
+            Err(Error::Api(ErrorResponse { code, .. })) if code == 404 => {
+                todo!("error retrieving resource lock %v: %v");
+                return false;
+            }
+            Err(_) => {
+                if let Ok(_) = self.cfg.lock.create(new_record.clone()).await {
+                    self.observed_record = new_record;
+                    self.observed_time = SystemTime::now();
+                    return true;
+                } else {
+                    todo!("error initially creating leader election record: %v");
+                    return false;
+                }
+            }
+            Ok(old_record) => {
+                // 2. Record obtained, check the Identity & Time
+                if self.observed_record != old_record {
+                    self.observed_record = old_record.clone();
+                    self.observed_time = SystemTime::now();
+                }
+                if old_record
+                    .holder_identity
+                    .map(|id| id.len() > 0)
+                    .unwrap_or_default()
+                    && self.observed_time + self.cfg.lease_duration > now
+                    && !self.is_leader()
+                {
+                    todo!("lock is held by %v and has not yet expired");
+                    return false;
+                }
+
+                // 3. We're going to try to update. The leaderElectionRecord is
+                // set to it's default here. Let's correct it before updating.
+                new_record.lease_transitions = old_record.lease_transitions;
+                if self.is_leader() {
+                    new_record.acquire_time = old_record.acquire_time;
+                } else {
+                    new_record.lease_transitions.map(|t| t + 1);
+                }
+
+                // Update the lock itself
+                if let Err(e) = self.cfg.lock.update(new_record.clone()).await {
+                    todo!("Failed to update lock: %v");
+                    return false;
+                }
+
+                self.observed_record = new_record;
+                self.observed_time = SystemTime::now();
+                return true;
+            }
+        }
     }
 
     fn report_transition_if_needed(&mut self) {
